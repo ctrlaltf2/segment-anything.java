e4d9dcfbc493ba3a9fab45378beacd5a7e2b9bd2
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-05-10 18:48:46 -0400

    Welp nd4j-onnxruntime is only on opset 2. Fix incoming

--- src/main/java/dev/troyer/sam/Main.java
+++ src/main/java/dev/troyer/sam/Main.java
@@ -1,11 +1,14 @@
 package dev.troyer.sam;
 
+import ai.onnxruntime.OrtException;
 import dev.troyer.sam.SamImage;
+import dev.troyer.sam.SamEncoder;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 import ai.onnxruntime.OnnxTensor;
 import ai.onnxruntime.OrtEnvironment;
@@ -16,7 +19,7 @@ import org.nd4j.linalg.factory.ops.NDImage;
 import org.nd4j.linalg.factory.Nd4j;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OrtException {
         String imagePath = "/home/caleb/Pictures/example.jpg";
         BufferedImage image = null;
 
@@ -26,7 +29,17 @@ public class Main {
             System.err.println("Error while loading image: " + e.getMessage());
             return;
         }
-        
+
         SamImage samImage = new SamImage(image);
+
+        try {
+            Path model_path = Path.of("./src/main/resources/data/vit_b/encoder-vit_b.quant.onnx");
+            SamEncoder encoder = new SamEncoder(model_path);
+            var out = encoder.forward(samImage);
+            System.out.println(out);
+        } catch (OrtException e) {
+            System.err.println("Error while loading model: " + e.getMessage());
+            return;
+        }
     }
 }
