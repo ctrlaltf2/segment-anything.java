f59d48da6fe1d1424178e2acafba13ffa43446b6
ctrlaltf2 <github@troyer.dev>
2024-01-22 23:10:49 -0500

    Use repo-based image for testing
    Thanks Mingjun Liu!

--- src/main/java/dev/troyer/sam/Main.java
+++ src/main/java/dev/troyer/sam/Main.java
@@ -12,11 +12,11 @@ import ai.onnxruntime.OrtException;
 
 public class Main {
     public static void main(String[] args) throws OrtException {
-        String imagePath = "/home/user/Pictures/example.jpg";
+        Path imagePath = Path.of("./test/assets/mingjun-liu-mVWqCdTHfxs-unsplash.jpg");
         BufferedImage image = null;
 
         try {
-            image = ImageIO.read(new File(imagePath));
+            image = ImageIO.read(new File(imagePath.toString()));
         } catch (IOException e) {
             System.err.println("Error while loading image: " + e.getMessage());
             return;
@@ -26,12 +26,13 @@ public class Main {
         SamImage samImage = new SamImage(image);
 
         try {
-            Path model_path = Path.of("./src/main/resources/data/vit_b/encoder-vit_b.quant.onnx");
-            SamEncoder encoder = new SamEncoder(env, model_path);
+            Path modelPath = Path.of("./src/main/resources/data/vit_b/encoder-vit_b.quant.onnx");
+            SamEncoder encoder = new SamEncoder(env, modelPath);
             var out = encoder.forward(env, samImage);
             System.out.println(out);
             var result = out.get(0);
             float[][][][] value = (float[][][][]) result.getValue(); // 1x256x64x64
+            var placeholder = out.get(0);
         } catch (OrtException e) {
             System.err.println("Error while loading model: " + e.getMessage());
             return;
