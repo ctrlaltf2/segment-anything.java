f888d9ac57924a39d594fed74aab50216f0b2e92
ctrlaltf2 <github@troyer.dev>
2024-01-22 23:00:35 -0500

    Bump onnxruntime to v0.16.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.microsoft.onnxruntime:onnxruntime:1.14.0")
+    implementation("com.microsoft.onnxruntime:onnxruntime:1.16.3")
     implementation("org.nd4j:nd4j:1.0.0-M2.1")
     implementation("org.nd4j:nd4j-native-platform:1.0.0-M2.1")
 }
